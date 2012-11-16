201a1e13ef6fe3d7653a6dbd4fc8375ae68e4ce7
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-11-16 12:07:47 -0700

    Added one more git remote

--- build/support/002_configuration/git.rb
+++ build/support/002_configuration/git.rb
@@ -1,7 +1,7 @@
 configs ={
   :git => {
     :user => 'iqmetrix2012',
-    :remotes => %w/yonderboi igor-toporet joem-iq kayone MichaelSitter soplee akrogsgaard ChristopherWolf dspeidel moons14 RyanMarcotte lindsaylayne TammyF herbchow jennyzwu woaksie TristanMoss tylerham MrHayato/,
+    :remotes => %w/Radicre yonderboi igor-toporet joem-iq kayone MichaelSitter soplee akrogsgaard ChristopherWolf dspeidel moons14 RyanMarcotte lindsaylayne TammyF herbchow jennyzwu woaksie TristanMoss tylerham MrHayato/,
     :repo => 'prep' 
   }
 }
