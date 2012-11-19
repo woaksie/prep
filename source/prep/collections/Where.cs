014cd2079fa872befb40f3130bd65a916e307896
Keivan Beigi <keivanb@iqmetrix.com>
2012-11-19 14:06:02 -0800

    Pushing new changes

--- source/prep/collections/Where.cs
+++ source/prep/collections/Where.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using prep.utility.filtering;
 
 namespace prep.collections
 {
-  public class Where<ItemToFilter>
-  {
-    public static void has_a(Func<object, object> func)
+    public class Where<ItemToFilter>
     {
-      throw new NotImplementedException();
+        public static Func<Movie, ProductionStudio> has_a(Func<Movie, ProductionStudio> func)
+        {
+            return func;
+        }
     }
-  }
+
+    public static class FilterExtention
+    {
+        public static IMatchAn<Movie> equal_to(this  Func<Movie, ProductionStudio> projection, ProductionStudio studio)
+        {
+            return new ConditionalMatch<Movie>(c => projection(c) == studio);
+        }
+    }
 }
